2d7dc4a00b559298b0cae107b936ac4eddb91988
sree <mail.sreejith.23@gmail.com>
2019-04-09 20:09:41 -0400

    initial commit

---
+++ Blue Apron/Login.swift
@@ -0,0 +1,36 @@
+//
+//  Login.swift
+//  Blue Apron
+//
+//  Created by Sreejith Thrivikraman on 2019-04-09.
+//  Copyright © 2019 Sreejith Thrivikraman. All rights reserved.
+//
+
+import UIKit
+import TransitionButton
+
+class Login: UIViewController
+
+{
+    
+    
+    @IBOutlet weak var Text_Password: UITextField!
+    @IBOutlet weak var Text_userName: UITextField!
+    @IBOutlet weak var button_login: UIButton!
+    
+    override func viewDidLoad()
+    {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+      
+       
+        button_login.layer.cornerRadius = 20
+        Text_userName.placeholder = "Username"
+        Text_Password.placeholder = "Password"
+        
+    }
+    
+
+   
+}
---
+++ Podfile
@@ -0,0 +1,11 @@
+# Uncomment the next line to define a global platform for your project
+# platform :ios, '9.0'
+
+target 'Blue Apron' do
+  # Comment the next line if you're not using Swift and don't want to use dynamic frameworks
+  use_frameworks!
+
+  # Pods for Blue Apron
+
+  pod 'TransitionButton'
+end
