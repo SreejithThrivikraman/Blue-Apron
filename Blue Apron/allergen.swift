7b8beb4d2dcab7ec5955e9469771cdcb15380025
sree <mail.sreejith.23@gmail.com>
2019-04-16 11:41:24 -0400

    updated with collection view

---
+++ Blue Apron/allergen.swift
@@ -0,0 +1,13 @@
+//
+//  allergen.swift
+//  Blue Apron
+//
+//  Created by Sreejith Thrivikraman on 2019-04-15.
+//  Copyright © 2019 Sreejith Thrivikraman. All rights reserved.
+//
+
+import UIKit
+
+class allergen: UICollectionViewCell {
+    
+}
