7f5519eeae3432891d560cfa58adc2f4ce4a5654
sree <mail.sreejith.23@gmail.com>
2019-04-17 19:07:40 -0400

    Multiple selection completed in UI

--- Blue Apron/diet_selection_image_and_label.swift
+++ Blue Apron/diet_selection_image_and_label.swift
@@ -11,10 +11,29 @@ import UIKit
 class diet_selection_image_and_label: UICollectionViewCell
 
 {
+    @IBOutlet weak var fadeImage: UIImageView!
     @IBOutlet weak var diet_image: UIImageView!
     @IBOutlet weak var diet_label: UILabel!
+    @IBOutlet weak var itemSelected: UIImageView!
     
-    
-
+    override var isSelected: Bool
+        {
+        didSet
+        {
+            if self.isSelected
+            {
+                itemSelected.isHidden = !isSelected
+                fadeImage.isHidden = !isSelected
+                
+            }
+            else
+            {
+                itemSelected.isHidden = true
+                fadeImage.isHidden = true
+                
+            }
+            
+        }
+    }
     
 }
