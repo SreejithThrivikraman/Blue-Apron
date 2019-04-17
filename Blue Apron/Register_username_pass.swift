0e611a224591a19070e41c381a87052f79269b27
sree <mail.sreejith.23@gmail.com>
2019-04-17 19:55:05 -0400

    Registartion complete

---
+++ Blue Apron/Register_username_pass.swift
@@ -0,0 +1,30 @@
+//
+//  Register_username_pass.swift
+//  Blue Apron
+//
+//  Created by Sreejith Thrivikraman on 2019-04-17.
+//  Copyright © 2019 Sreejith Thrivikraman. All rights reserved.
+//
+
+import UIKit
+
+class Register_username_pass: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
