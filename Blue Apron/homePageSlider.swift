9b00d6c3dd5254d5d685adc2627fd30e2565a887
sree <mail.sreejith.23@gmail.com>
2019-04-18 23:21:32 -0400

    Main page started

---
+++ Blue Apron/homePageSlider.swift
@@ -0,0 +1,30 @@
+//
+//  homePageSlider.swift
+//  Blue Apron
+//
+//  Created by Sreejith Thrivikraman on 2019-04-18.
+//  Copyright © 2019 Sreejith Thrivikraman. All rights reserved.
+//
+
+import UIKit
+
+class homePageSlider: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
