25c81c99a6974e14cce2552f91125b0a5448fbae
sree <mail.sreejith.23@gmail.com>
2019-04-22 19:07:33 -0400

    Recipe page design completed

---
+++ Blue Apron/DishImageClassCollectionViewCell.swift
@@ -0,0 +1,13 @@
+//
+//  DishImageClassCollectionViewCell.swift
+//  Blue Apron
+//
+//  Created by Sreejith Thrivikraman on 2019-04-22.
+//  Copyright © 2019 Sreejith Thrivikraman. All rights reserved.
+//
+
+import UIKit
+
+class DishImageClassCollectionViewCell: UICollectionViewCell {
+    
+}
---
+++ Blue Apron/recipePool.swift
@@ -0,0 +1,30 @@
+//
+//  recipePool.swift
+//  Blue Apron
+//
+//  Created by Sreejith Thrivikraman on 2019-04-22.
+//  Copyright © 2019 Sreejith Thrivikraman. All rights reserved.
+//
+
+import UIKit
+
+class recipePool: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
