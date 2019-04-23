0baa8bcf36a8b03e71ad4632d31a92dc494f46e4
sree <mail.sreejith.23@gmail.com>
2019-04-23 00:45:56 -0400

    created dish details UI

---
+++ Blue Apron/userDishPool.swift
@@ -0,0 +1,13 @@
+//
+//  userDishPool.swift
+//  Blue Apron
+//
+//  Created by Sreejith Thrivikraman on 2019-04-22.
+//  Copyright © 2019 Sreejith Thrivikraman. All rights reserved.
+//
+
+import UIKit
+
+class userDishPool: UICollectionViewCell {
+    
+}
