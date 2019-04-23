0e642fdea70bbad33e2c15da02fedb648ba743a0
sree <mail.sreejith.23@gmail.com>
2019-04-23 14:02:53 -0400

    Completed login and signup

--- Blue Apron/Register_username_pass.swift
+++ Blue Apron/Register_username_pass.swift
@@ -7,6 +7,8 @@
 //
 
 import UIKit
+import Firebase
+import FirebaseAuth
 
 class Register_username_pass: UIViewController {
 
@@ -26,5 +28,36 @@ class Register_username_pass: UIViewController {
         // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func signupAction(_ sender: Any)
+    {
+        if userName.text == "" {
+            let alertController = UIAlertController(title: "Error", message: "Please enter your email and password", preferredStyle: .alert)
+            
+            let defaultAction = UIAlertAction(title: "OK", style: .cancel, handler: nil)
+            alertController.addAction(defaultAction)
+            
+            present(alertController, animated: true, completion: nil)
+            
+        } else {
+            Auth.auth().createUser(withEmail: userName.text!, password: password.text!) { (user, error) in
+                
+                if error == nil {
+                    print("You have successfully signed up")
+                    //Goes to the Setup page which lets the user take a photo for their profile picture and also chose a username
+                    
+                    let vc = self.storyboard?.instantiateViewController(withIdentifier: "Home")
+                    self.present(vc!, animated: true, completion: nil)
+                    
+                } else {
+                    let alertController = UIAlertController(title: "Error", message: error?.localizedDescription, preferredStyle: .alert)
+                    
+                    let defaultAction = UIAlertAction(title: "OK", style: .cancel, handler: nil)
+                    alertController.addAction(defaultAction)
+                    
+                    self.present(alertController, animated: true, completion: nil)
+                }
+            }
+        }
+    }
+    
 }
